374284c718e45f065dd84d8223ac8d203eb372d1
win7 <win7@win7-PC>
2018-05-22 15:20:32 +0200

    module 3 finished

---
+++ 02_types_statements__combining data types/12_other_data_types.py
@@ -0,0 +1,25 @@
+'''
+Created on 22 may. 2018
+
+@author: win7
+'''
+
+if __name__ == '__main__':
+    
+    #Other important data types
+    
+    #complex
+    
+    #byte, bytearray
+    
+    #like lists but they cannot change their value
+    tuple (3, 5, 6, "Mark")
+    
+    #set and frozenset
+        #set is like a list but they only hold unique objects
+        #set ([3, 4, 3, 3, 35, 5, 34, 3])
+        
+        
+    
+    
+    pass
