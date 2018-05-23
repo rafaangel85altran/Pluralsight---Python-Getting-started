7fd97947f8ed003469d11f2e87f5e453921c8054
win7 <win7@win7-PC>
2018-05-23 14:26:56 +0200

    Homework, make an algorith to ask student names and IDsand then print

--- 04_Functions_files_yield_and_lambda/04_01_functions.py
+++ 04_Functions_files_yield_and_lambda/04_01_functions.py
@@ -10,43 +10,36 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
+
 def print_students_titlecase():
-    students_titlecase = get_students_titlecase()
-    print (students_titlecase)
+    #students_titlecase = get_students_titlecase()
+    print (students)
+    
     
 def add_student(name, student_id=332):
     student = {"name" : name, "student_id" : student_id}      
-    students.append(student)       
-    
+    students.append(student)
+        
 student_list = get_students_titlecase()
     
-add_student(name="Johny", student_id=15)
+print("##### Welcome to the add_students program #####")    
+response = input("Do you want to add a user to your list? (Y/N)")
+
+while response == "Y":
+    student_name = input("Enter student name: ")
+    student_id = input("Enter student ID: ")
+    add_student(student_name, student_id)
+    response = input("Do you still want to add more students? (Y/N)")
     
-#If I try to do this
-    #print(name)            #unresolved reference "name"
-    
-"""def add_student(name, student_id=332):   #By adding "=332" (value of number doesn´t matter)
-    students.append(name, student_id)       # we´re declaring this argument 332 as optional
-"""
+print_students_titlecase()
 
 """
-There is a type of function with a variable number of arguments like print(arg1, arg2, arg3, ..., argn)
-
-def var_args(name, *args):
-        print(name)
-        print(args)
-        
-var_args("Mark", None, True, 483240932, "Loves Python")
-
+HOMEWORK: Ask the user if we wants to add a new student to the list, if yes then enter a new name 
+and id and sak again until the user donesn´t want to, then the program will print the entire list
+of students entered so far 
 """
 
-"""This is a veri flexible way to add a variable number of arguments to any function
-def var_args(name, **kwargs):
-        print(name)
-        print(kwargs["description"], kwargs["feedback"], kwargs["pluralsight_subscriber"])
-        
-var_args("Mark", description="Loves Python", feedback=None, pluralsight_subscriber=True)
-"""
+
