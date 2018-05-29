71777a2cea140fdc327930aba2c5dfac86e4e161
win7 <win7@win7-PC>
2018-05-29 13:57:03 +0200

    ludmila is here

--- 04_Functions_files_yield_and_lambda/04_01_functions.py
+++ 04_Functions_files_yield_and_lambda/04_01_functions.py
@@ -6,6 +6,12 @@ A function is a block of organice and reusable code which is used to perform a c
 @author: win7
 '''
 
+"""
+HOMEWORK: Ask the user if we wants to add a new student to the list, if yes then enter a new name 
+and id and sak again until the user donesn´t want to, then the program will print the entire list
+of students entered so far and check if some student_id is duplicated
+"""
+
 students = []
 
 def students_id_check():
@@ -31,17 +37,36 @@ def print_students_dict_org():
     for x in students:
         print (x)  
 
-"""
-def print_students_titlecase():
-    #students_titlecase = get_students_titlecase()
-    print (students)
-""" 
     
 def add_student(name, student_id=332):
     student = {"name" : name, "student_id" : student_id}      
     students.append(student)
+
+"""
+ADD function to store the data in a txt file
+"""
+
+def save_file(student):
+    try:
+        f = open("students.txt", "a")       # Append or create a new file
+        for x in student:
+            f.write(x + "\n")               # 
+        f.close()
+    except TypeError as error:
+        print("File could not be saved")
+        print(error)
         
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in f.readlines():
+            add_student(student)
+        f.close()
+    except Exception:
+        print("File could not be read")
+    
 student_list = get_students_titlecase()
+
 
 print("##### Welcome to the add_students program #####")    
 response = input("Do you want to add a user to your list? (Y/N)")
@@ -55,14 +80,13 @@ while response == "Y":
     
 if students_id_check() == True:
     print("Carefull, at least one student id is duplitaced, please check it out")
-
+else:
+    save_file(students)
+    
 print_students_dict_org()
+read_file()
 
 
-"""
-HOMEWORK: Ask the user if we wants to add a new student to the list, if yes then enter a new name 
-and id and sak again until the user donesn´t want to, then the program will print the entire list
-of students entered so far and check if some student_id is duplicated
-"""
 
+    
 
